79e75908ee6a59d55699ac7666eea33531a53f6e
Deven <dmaheshwari20@stuy.edu>
2021-10-10 15:09:00 -0400

    Fixed html code creating table on flask and app

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -4,17 +4,36 @@
 # October 8 2021
 
 from flask import Flask, render_template 
+import occupation
+import csv
+import random
+
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello_world():
     return "No hablo queso!"
 
-coll = [0,1,1,2,3,5,8]
-
 @app.route("/occupyflaskst") 
 def test_tmplt():
-    return render_template( 'model_tmplt.html', foo="fooooo", collection=coll)
+    jobp = {}
+    with open('data/occupations.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            job = row["Job Class"]
+            percentage = float(row["Percentage"])
+            jobp[job] = str(percentage)
+        del jobp['Total']
+    return render_template( 'tablified.html', foo = "K13", random = chooseRandom(jobp), response = jobp)
+
+def chooseRandom(jobdict):     #
+    randVal = random.uniform(0, 100) # randomly chooses a number from 0 to total percentage
+    for k, v in jobdict.items():
+        randVal -= float(v)
+        if randVal <= 0:    # first cumulative percentage >= random value is the job chosen
+            return k
 
 if __name__ == "__main__":
     app.debug = True
