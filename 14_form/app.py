1745f78b259287c4ff89d0dcba1b4551c01bdbad
Deven <dmaheshwari20@stuy.edu>
2021-10-15 08:13:41 -0400

    Updated app.py
    Worked on my local machine last night but testing on school computers not working

--- 14_form/app.py
+++ 14_form/app.py
@@ -52,7 +52,7 @@ def authenticate(): # Manipulation of form data. Prediction: Will work - does no
     #print(request.args['username']) # Test by uncommenting line by line and seeing outputs in terminal and app. Prediction: Depends on user input
     print("***DIAG: request.headers ***")
     print(request.headers) # Headers is attribute of request
-    if (request.method == "POST"):
+    if (request.method == "GET"):
         return render_template( 'response.html', name = request.args['username'] )
     return render_template( 'response.html' )  #response to a form submission
 
