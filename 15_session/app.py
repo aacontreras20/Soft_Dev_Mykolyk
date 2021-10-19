2ffa37b95d0e4489b33abe1210004aec7511a00d
Deven <dmaheshwari20@stuy.edu>
2021-10-18 20:38:17 -0400

    Updated username

--- 15_session/app.py
+++ 15_session/app.py
@@ -13,8 +13,10 @@ from flask.globals import session           #facilitate form submission
 
 app = Flask(__name__)    #create Flask object
 app.secret_key = "K15"
+username = 'Sports!' 
+password = 'Mykolyk'
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage(): # Display. Prediction: Will work, login.html is in templates directory. 
     print("\n\n\n") # New line. HTML uses <br> tags
     print("***DIAG: this Flask obj ***")
@@ -23,8 +25,9 @@ def disp_loginpage(): # Display. Prediction: Will work, login.html is in templat
     print(request) # From form submission
     print("***DIAG: request.args ***")
     print(request.args) # Inputs from user
-    session['username'] = 'Sports!'
-    session['password'] = 'Mykolyk'
+    if request.method == 'POST':
+        session['username'] = request.cookies.get['username']
+        session['password'] = request.cookies.get['password']
     #print("***DIAG: request.args['username']  ***") # Test by uncommenting line by line and seeing outputs in terminal and app. Prediction: Will work, just printing to termianl.  
     #print(request.args['username']) # Test by uncommenting line by line and seeing outputs in terminal and app. # Prediction: Depends on user input
     print("***DIAG: request.headers ***")
@@ -32,7 +35,7 @@ def disp_loginpage(): # Display. Prediction: Will work, login.html is in templat
     return render_template( 'login.html' )
 
 
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET', 'POST'])
 def authenticate(): # Manipulation of form data. Prediction: Will work - does not reference external files. 
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -49,7 +52,11 @@ def authenticate(): # Manipulation of form data. Prediction: Will work - does no
         return render_template( 'response.html', name = request.args['username'], url = request.args['sub1'] )
     return render_template( 'response.html' )  #response to a form submission
 
-
+@app.route("/error") # , methods=['GET', 'POST'])
+def prompt(): # Manipulation of form data. Prediction: Will work - does not reference external files. 
+    if (request.method == "GET"):
+        return render_template( 'response.html', name = request.args['username'], url = request.args['sub1'] )
+    return render_template( 'response.html' )  #response to a form submission
     
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
